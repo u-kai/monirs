9da6a40704a029e624c9eca1a86966890dcea54c
u-kai <76635578+u-kai@users.noreply.github.com>
2022-10-20 22:25:02 +0900

    change filestore  time -> filesize

--- src/filestore.rs
+++ src/filestore.rs
@@ -1,9 +1,9 @@
 use std::collections::HashMap;
 
-type FileModifyTime = u128;
+type FileSize = u128;
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct FileStore {
-    store: HashMap<String, FileModifyTime>,
+    store: HashMap<String, FileSize>,
 }
 impl FileStore {
     pub fn new() -> Self {
@@ -14,20 +14,20 @@ impl FileStore {
     pub fn is_new(&self, path: &str) -> bool {
         !self.store.contains_key(path)
     }
-    pub fn is_modify(&self, path: &str, time: FileModifyTime) -> bool {
-        if let Some(old_time) = self.store.get(path) {
-            return old_time != &time;
+    pub fn is_modify(&self, path: &str, filesize: FileSize) -> bool {
+        if let Some(old_filesize) = self.store.get(path) {
+            return old_filesize != &filesize;
         }
         false
     }
-    pub fn update(&mut self, path: String, time: FileModifyTime) {
-        if self.is_modify(&path, time) {
-            self.store.insert(path, time);
+    pub fn update(&mut self, path: String, filesize: FileSize) {
+        if self.is_modify(&path, filesize) {
+            self.store.insert(path, filesize);
         }
     }
-    pub fn insert(&mut self, path: String, time: FileModifyTime) {
+    pub fn insert(&mut self, path: String, filesize: FileSize) {
         if self.is_new(&path) {
-            self.store.insert(path, time);
+            self.store.insert(path, filesize);
         }
     }
 }
